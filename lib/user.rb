a91bd4294f3dcc40ac75457fd19f9d03a954d705
Collin Reisdorf <creisdorf@toth.com>
2012-02-21 13:21:47 -0500

    beginning to add post model for reddit integration

--- lib/user.rb
+++ lib/user.rb
@@ -19,4 +19,28 @@ class User < CouchRest::Model::Base
       self.slug = nickname.downcase.gsub(/[^a-z0-9]/,'-').squeeze('-').gsub(/^-|-$/,'') if new?
     end
   end
+  
 end
+
+class Post < CouchRest::Model::Base
+  use_database $COUCH.default_database
+
+  property :uid # User ID who posted it
+  property :pid # Post ID
+  property :url # Page URL, stripped of weird string stuff when possible.
+  property :full_url
+  property :reddit_url # Reddit Resource Link
+  timestamps!
+  
+  design do 
+    view :by_pid
+  end
+  
+  def clear_url
+    # if self.slug has weird date related querystring info 
+    #   save it without it
+    # end
+  end
+  
+end
+
